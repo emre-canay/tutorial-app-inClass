373102987b7b3b669bdda14da2df9de60e12ace0
emre-canay <emre.software55@gmail.comgit config --listgit config --global user.email emre.software55@gmail.com>
2023-02-28 01:33:24 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+    const { data } = await axios(BASE_URL);
+    setTutorials(data);
+  };
+  console.log(tutorials);
+  //? componentDidMount
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  return (
+    <>
+      <AddTutorial />
+      <TutorialList tutorials={tutorials} />
+    </>
+  );
+};
+
+export default Home;
